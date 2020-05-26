3fa4496bce435ec7ca3a2b5259767e38ea99dc6e
avner <avner.moshkovitz@gmail.com>
2020-05-25 23:07:58 -0700

    first commit

---
+++ js2.js
@@ -0,0 +1,11 @@
+
+class Es6Module {
+    static myFunction2 = function(val2) {
+        console.log('BEG myFunction2');
+        console.log('val2', val2); 
+        return 3;
+    };
+};
+
+export { Es6Module };
+
